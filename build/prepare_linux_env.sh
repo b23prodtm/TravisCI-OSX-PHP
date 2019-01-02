ea3960c233e1a904ba105113144f4a7badf2e46b
Mac Bok Arimana <b23prodtm@users.sourceforge.net>
2019-01-02 22:41:42 +0100

    git phpenv

--- build/prepare_linux_env.sh
+++ build/prepare_linux_env.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 echo "Here's the Linux environment:"
+echo "Adding php support"
+curl -L https://raw.githubusercontent.com/phpenv/phpenv-installer/master/bin/phpenv-installer \
+    | PHPENV_ROOT=/usr/local/bin/phpenv bash
+export PATH="${PHPENV_ROOT}/bin:${PATH}"
+eval "$(phpenv init -)"
 phpenv global ${_PHP}
-composer self-update
 phpenv --version
-composer --version
--- build/prepare_windows_env.sh
+++ build/prepare_windows_env.sh
@@ -9,12 +9,12 @@ choco upgrade
 if [[ "${_PHP}" == "hhvm" ]]; then
     echo "Cannot install unsupported HHVM dependencies."
 fi
-echo "Adding curl support"
-choco install curl
 echo "Adding openssl support"
 choco install openssl.light
 echo "Adding php support"
+curl -L https://raw.githubusercontent.com/phpenv/phpenv-installer/master/bin/phpenv-installer \
+    | PHPENV_ROOT=/usr/local/bin/phpenv bash
+export PATH="${PHPENV_ROOT}/bin:${PATH}"
+eval "$(phpenv init -)"
 choco install php --version ${_PHP} -my
-export PATH=/c/tools/php$(echo ${_PHP} | cut -c 1,3):$PATH
-git clone git://github.com/phpenv/phpenv.git ~/.phpenv
-echo 'export PATH="$HOME/.phpenv/bin:$PATH"' >> ~/.bash_profile
+export PATH=/c/tools/php$(echo ${_PHP} | cut -c 1,3):${PATH}
