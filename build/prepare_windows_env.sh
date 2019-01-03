81833261940cef3cfa618cce86445a6cf29a3f7f
Mac Bok Arimana <b23prodtm@users.sourceforge.net>
2019-01-03 21:55:36 +0100

    WIP prepare phpenv

--- build/prepare_windows_env.sh
+++ build/prepare_windows_env.sh
@@ -14,7 +14,12 @@ choco install openssl.light
 echo "Adding php support"
 curl -L https://raw.githubusercontent.com/phpenv/phpenv-installer/master/bin/phpenv-installer \
     | PHPENV_ROOT=/usr/local/bin/phpenv bash
-export PATH="${PHPENV_ROOT}/bin:${PATH}"
-eval "$(phpenv init -)"
+echo "
+export PHPENV_ROOT=\"/usr/local/bin/phpenv\"\n
+   if [ -d \"\${PHPENV_ROOT}\" ]; then\n
+      export PATH=\${PHPENV_ROOT}/bin:\${PATH}\n
+     eval \"\$(phpenv init -)\"\n
+   fi" >> ~/.bash_profile
+exec $SHELL -l
 choco install php --version ${_PHP} -my
 export PATH=/c/tools/php$(echo ${_PHP} | cut -c 1,3):${PATH}
