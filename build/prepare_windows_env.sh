06445e58a99f6b29efd49252485d8095bb837682
Mac Bok Arimana <b23prodtm@users.sourceforge.net>
2019-01-12 00:06:35 +0100

    --with-openssl

--- build/prepare_windows_env.sh
+++ build/prepare_windows_env.sh
@@ -3,7 +3,7 @@
 echo "Here's the Windows environment:"
 echo "Fixing phpenv support"
 export PATH="/c/Program Files/git${PHPENV_ROOT}/libexec:${PATH}"
-eval "$(/c/Program Files/git${PHPENV_ROOT}/libexec/phpenv init -)"
+# eval "$(phpenv init -)"
 
 choco --version
 echo "Updating choco..."
@@ -12,7 +12,7 @@ choco upgrade
 if [[ "${_PHP}" == "hhvm" ]]; then
     echo "Cannot install unsupported HHVM dependencies."
 fi
-choco install php --version ${_PHP} -my
+choco install php --version ${_PHP} -my --with-openssl
 export PATH="/c/tools/php$(echo ${_PHP} | cut -c 1,3):${PATH}"
 
 echo "Adding openssl support"
