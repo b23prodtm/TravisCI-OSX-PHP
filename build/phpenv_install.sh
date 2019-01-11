39c5215d3ad002b6bea43516dfcea9740f026c86
Mac Bok Arimana <b23prodtm@users.sourceforge.net>
2019-01-11 23:22:42 +0100

    PHPenv Update .travis.yml

--- build/phpenv_install.sh
+++ build/phpenv_install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-echo "Adding php support"
-curl -L https://raw.githubusercontent.com/phpenv/phpenv-installer/master/bin/phpenv-installer | PHPENV_ROOT=/usr/local/bin/phpenv bash
+echo "Adding phpenv support"
+curl -L https://raw.githubusercontent.com/phpenv/phpenv-installer/master/bin/phpenv-installer | bash
 echo "
-export PHPENV_ROOT=\"/usr/local/bin/phpenv\"
+export PHPENV_ROOT=\"${PHPENV_ROOT}\"
 if [ -d \"\${PHPENV_ROOT}\" ]; then
-  export PATH=\${PHPENV_ROOT}/bin:\${PATH}
+  export PATH=\"\${PHPENV_ROOT}/bin:\${PATH}\"
   eval \"\$(phpenv init -)\"
 fi" >> ~/.bash_profile
-exec $SHELL -l
+source ~/.bash_profile
+# exec $SHELL -l #; uncompatible in CI bash
--- build/prepare_windows_env.sh
+++ build/prepare_windows_env.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Here's the Windows environment:"
+echo "Fixing phpenv support"
+export PATH="/c/Program Files/git${PHPENV_ROOT}/libexec:${PATH}"
 choco --version
 
 echo "Updating choco..."
@@ -9,7 +11,8 @@ choco upgrade
 if [[ "${_PHP}" == "hhvm" ]]; then
     echo "Cannot install unsupported HHVM dependencies."
 fi
+choco install php --version ${_PHP} -my
+export PATH="/c/tools/php$(echo ${_PHP} | cut -c 1,3):${PATH}"
+
 echo "Adding openssl support"
 choco install openssl.light
-choco install php --version ${_PHP} -my
-export PATH=/c/tools/php$(echo ${_PHP} | cut -c 1,3):${PATH}
