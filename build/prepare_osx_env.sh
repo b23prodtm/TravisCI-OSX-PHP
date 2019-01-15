6f26009576f3ee76980e1c343e161a97d02399c6
Tiana Rakoto Arimanana <5566338+b23prodtm@users.noreply.github.com>
2019-01-15 21:23:13 +0100

    Update prepare_osx_env.sh

--- build/prepare_osx_env.sh
+++ build/prepare_osx_env.sh
@@ -16,3 +16,4 @@ brew upgrade openssl
 export PATH="/usr/local/opt/openssl/bin:$PATH"
 export LDFLAGS="$LDFLAGS -L/usr/local/opt/openssl/lib"
 export CPPFLAGS="$CPPFLAGS -I/usr/local/opt/openssl/include"
+export PKG_CONFIG_PATH="/usr/local/opt/curl-openssl/lib/pkgconfig"
