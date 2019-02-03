749bf5ab723d392ba68c1ea2ebcde9622368c48b
b23production GNU <b23prodtm@users.sourceforge.net>
2019-02-03 13:03:36 +0100

    Update prepare_osx_env.sh

--- build/prepare_osx_env.sh
+++ build/prepare_osx_env.sh
@@ -17,4 +17,8 @@ export PATH="/usr/local/opt/openssl/bin:$PATH"
 export LDFLAGS="$LDFLAGS -L/usr/local/opt/openssl/lib"
 export CPPFLAGS="$CPPFLAGS -I/usr/local/opt/openssl/include"
 export PKG_CONFIG_PATH="/usr/local/opt/curl-openssl/lib/pkgconfig"
+echo "Downloading cacert.pem..."
+curl -sSL http://curl.haxx.se/ca/cacert.pem >> cacert.pem
+mkdir -p /usr/local/etc/openssl/certs
+mv -v cacert.pem /usr/local/etc/openssl/certs
 export ADDITIONAL_PHP_INI=build/.travis_osx.php.ini
