0fb8d3045b109595f261fedb0315abdcbbf82862
Tiana Rakoto Arimanana <5566338+b23prodtm@users.noreply.github.com>
2020-05-02 16:00:09 +0200

    Fixes: /etc/apt/trusted.gpg.d/php.gpg: Permission denied

--- build/prepare_linux_env.sh
+++ build/prepare_linux_env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" \
-		| tee "/etc/apt/sources.list.d/php${_PHP}.list"
+		| sudo tee "/etc/apt/sources.list.d/php${_PHP}.list"
 echo "Here's the Linux environment:"
 phpenv versions
 export ADDITIONAL_PHP_INI=build/.travis_linux.php.ini
