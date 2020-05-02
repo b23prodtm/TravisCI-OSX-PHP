4c07233f0f1b31790666c610a64a8248175d2bf3
Tiana Rakoto Arimanana <5566338+b23prodtm@users.noreply.github.com>
2020-05-02 11:20:13 +0200

    Fixes: shell

--- build/handle_linux_pkg.sh
+++ build/handle_linux_pkg.sh
@@ -1,33 +1,20 @@
 #!/usr/bin/env bash
 if [[ "$#" > 0 ]]; then
-    echo "Handling \"$1\" brew package..."
+    echo "Handling \"$1\" apt package..."
 else
-    echo "Brew failed - invalid $0 call"
+    echo "Apt failed - invalid $0 call"
     exit 1;
 fi
 pkg=$1
 if [[ "$#" > 1  && $2 != "latest" ]]; then
   pkg="${pkg}@$2"
 fi
-if [[ $(brew ls --versions "${pkg}") ]]; then
-    if brew outdated "${pkg}"; then
-        echo "Package upgrade is not required, skipping"
-    else
-        echo "Updating package...";
-        brew upgrade "${pkg}"
-        if [ $? -ne 0 ]; then
-            echo "Upgrade failed"
-            exit 1
-        fi
-    fi
-else
-    echo "Package not available - installing..."
-    brew install "${pkg}"
-    if [ $? -ne 0 ]; then
-        echo "Install failed"
-        exit 1
-    fi
-fi
-
-echo "Linking installed package..."
-brew link --force "${pkg}"
+DEBIAN_FRONTEND="noninteractive"
+apt-get update && \
+apt-get install -y \
+ $pkg && \
+echo "**** cleanup ****" && \
+rm -rf \
+ /tmp/* \
+ /var/lib/apt/lists/* \
+ /var/tmp/*
